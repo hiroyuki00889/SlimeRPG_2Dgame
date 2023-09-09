c78720917d6724ee012e6e2eb020dc4a1c1d019f
hiroyuki00889 <murasansei8_13@yahoo.co.jp>
2023-09-09 11:02:59 +0900

    シーン1編集、Animation・NDialogのPrefab・スクリプト追加（前回より作業少なくしてます

---
+++ Assets/Script/AnimateNDialog.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AnimateNDialog : MonoBehaviour
+{
+    [SerializeField] private Animator m_Animator;
+    [SerializeField] private int layer;
+    [SerializeField] FirstEvent firstEvent;
+    //[SerializeField] FirstEnemy firstEnemy;
+    // IsOpenフラグ(アニメーターコントローラー内で定義したフラグ)
+    private static readonly int ParamIsOpen = Animator.StringToHash("IsOpen");
+    // ダイアログは開いているかどうか
+    public bool IsOpen => gameObject.activeSelf;
+    // アニメーション中かどうか
+    public bool IsTransition { get; private set; }
+    public bool enterTrigger = false;
+
+    private void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //衝突した相手のタグがEventsかつ、1度きりのフラグがオンになっているか
+        if (collision.gameObject.CompareTag("Events") && firstEvent.isFirstEvent == true
+            )
+        {
+            DialogNarratorOpen();
+        }
+    }
+    private void Update()
+    {
+        //ナレーターダイアログがアクティブかつ、エンターキーでナレーターの文章を進めるトリガー設定
+        if (Input.GetKeyDown(KeyCode.Return) && gameObject.activeSelf == true)
+        {
+            enterTrigger = true;
+        }
+        //
+        if(enterTrigger == true && firstEvent.isFirstEvent == false) 
+        {
+            DialogNarratorClose();
+            enterTrigger = false;
+        }
+    }
+
+    private void DialogNarratorOpen()
+    {
+            // 不正操作防止
+            if (IsOpen || IsTransition) return;
+            // DialogNarrator自体をアクティブにする
+            gameObject.SetActive(true);
+            // IsOpenフラグをtrueにセット
+            m_Animator.SetBool(ParamIsOpen, true);
+            // アニメーション待機
+            StartCoroutine(WaitAnimation("Shown"));
+    }
+
+    private void DialogNarratorClose()
+    {   
+         if (!IsOpen || IsTransition) return;
+       　// IsOpenフラグをfalseにセット
+         m_Animator.SetBool(ParamIsOpen, false);
+         // アニメーション待機し、終わったらパネル自体を非アクティブにする
+         StartCoroutine(WaitAnimation("Hidden", () => gameObject.SetActive(false)));
+    }
+
+    private IEnumerator WaitAnimation(string stateName, UnityAction onCompleted = null)
+    {
+        //このブール値がtrueの間は上2つの関数が動かない
+        IsTransition = true;
+
+        yield return new WaitUntil(() =>
+        {
+            //ステートが変化し、アニメーションが終了するまで待機
+            var state = m_Animator.GetCurrentAnimatorStateInfo(layer);
+            return state.IsName(stateName) && state.normalizedTime >= 1;
+        });
+
+        IsTransition = false;
+        onCompleted?.Invoke();
+    }
+}
---
+++ Assets/Script/FirstEvent.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FirstEvent : MonoBehaviour
+{
+    [SerializeField] AnimateNDialog animateNDialog;
+    bool isCallOnece = false;
+    public Text firstEventText;
+    public bool isFirstEvent = false;
+    int i = 0;
+
+    string[] words = {"魔物の縄張りに住むスライムは平和に暮らしていました",
+        "しかし、あることで縄張りを治める四天王にムカついてしまいました","スライムは四天王を倒す旅に出ます"};
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        //一度しか呼び出されないフラグがfalseかつ、衝突してきた相手がPlayer
+        if (!isCallOnece && collider.gameObject.CompareTag("Player"))
+        {
+            isCallOnece = true;
+            firstEventText.text = words[i];
+            isFirstEvent = true;
+        }
+    }
+
+    private void Update()
+    {
+        //ナレーターダイアログが開いている間、エンターキーを押すと、文章送り
+        if(animateNDialog.IsOpen && animateNDialog.enterTrigger == true)
+        {
+            i++;
+            firstEventText.text = words[i];
+
+            //最後の文章になって、エンターキーを押すとフラグオフ、Textオブジェクトを非アクティブにする
+            if(i >= words.Length && animateNDialog.enterTrigger == true) 
+            {
+                isFirstEvent = false;
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
