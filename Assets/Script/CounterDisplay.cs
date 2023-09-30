df6f55518826858281f12c4f94d9aab45f177aea
Valkyrja <scala.valkyrja@gmail.com>
2023-09-30 09:23:46 +0900

    UIカウンター

--- Assets/Script/CounterDisplay.cs
+++ Assets/Script/CounterDisplay.cs
@@ -8,6 +8,8 @@ public class CounterDisplay : MonoBehaviour
     public EnemyTagCounter enemyTagCounter;
     public Text counterTextA; // カウンターAを表示するUIテキスト
     public Text counterTextB; // カウンターBを表示するUIテキスト
+    public Image Club;
+    public Image Axe;
 
 
 
@@ -17,15 +19,42 @@ public class CounterDisplay : MonoBehaviour
         Dictionary<string, int> counters = enemyTagCounter.GetAllCounters();
 
         // カウンターの値をUIテキストに表示
+
         foreach (var kvp in counters)
         {
             if (kvp.Key == "EnemyTagA")
             {
-                counterTextA.text = $"EnemyTagA: {kvp.Value}";
+                if (kvp.Value == 0)
+                {
+                    Club.gameObject.SetActive(false); // 画像を非表示
+                    counterTextA.gameObject.SetActive(false);  // テキストを非表示
+                }
+                else if (kvp.Value >= 1)
+                {
+                    Club.gameObject.SetActive(true);  // 画像を表示
+                    counterTextA.gameObject.SetActive(true);   // テキストを表示
+
+                    // テキストに数値を表示
+                    counterTextA.text = kvp.Value.ToString();
+                }
+                // counterTextA.text = $"EnemyTagA: {kvp.Value}";
             }
             else if (kvp.Key == "EnemyTagB")
             {
-                counterTextB.text = $"EnemyTagB: {kvp.Value}";
+                if (kvp.Value == 0)
+                {
+                    Axe.gameObject.SetActive(false); // 画像を非表示
+                    counterTextB.gameObject.SetActive(false);  // テキストを非表示
+                }
+                else if (kvp.Value >= 1)
+                {
+                    Axe.gameObject.SetActive(true);  // 画像を表示
+                    counterTextB.gameObject.SetActive(true);   // テキストを表示
+
+                    // テキストに数値を表示
+                    counterTextB.text = kvp.Value.ToString();
+                }
+                // counterTextB.text = $"EnemyTagB: {kvp.Value}";
             }
             // 他のタグに対する表示も追加できます
         }
