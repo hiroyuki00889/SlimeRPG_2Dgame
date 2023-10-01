54881eb3f64c3116473eae06b0b5a6a8c52d1871
Valkyrja <scala.valkyrja@gmail.com>
2023-10-01 09:05:49 +0900

    no message

--- Assets/Script/UIManager.cs
+++ Assets/Script/UIManager.cs
@@ -1,18 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Image image;
+    public Text text;
+    public int x;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // xの値に応じてUI要素を表示または非表示にする
+        if (x == 0)
+        {
+            image.gameObject.SetActive(false); // 画像を非表示
+            text.gameObject.SetActive(false);  // テキストを非表示
+        }
+        else if (x >= 1)
+        {
+            image.gameObject.SetActive(true);  // 画像を表示
+            text.gameObject.SetActive(true);   // テキストを表示
+
+            // テキストに数値を表示
+            text.text = x.ToString();
+        }
     }
 }
