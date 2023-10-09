020922b3d3116af284c2950695334f43efd0a3d8
斎藤 <2201100257mn@cyber-u.ac.jp>
2023-10-09 19:45:14 +0900

    BatMove修正

--- Assets/Script/BatMove.cs
+++ Assets/Script/BatMove.cs
@@ -39,6 +39,7 @@ public class BatMove : MonoBehaviour
         else 
         {
             rb.Sleep();
+            move= false;   
         }
     }
 
@@ -48,7 +49,7 @@ public class BatMove : MonoBehaviour
         Instantiate(ultraSounds, this.transform.position, Quaternion.identity);
         //アニメーション終わるまで待つ処理,または後隙の設定
         Debug.Log("Magic");
-        Invoke("Punish", 2f);
+        Invoke("Punish", 1f);
     }
 
     private void Punish()
@@ -56,18 +57,35 @@ public class BatMove : MonoBehaviour
         punish = false;
         //ここに次の動きの処理
         time = 0;
-        rb.velocity = new Vector2(Random.Range(-3,4),Random.Range(-3,4));
+        rb.velocity = new Vector2(3*RandomMove(),3*RandomMove());
         Debug.Log("Punish");
+    }
+
+
+    private int RandomMove() 
+    {
+        int i = Random.Range(-1, 2);
+        if (i == -1)
+        {
+            return -1;
+        }
+        else if (i == 0)
+        {
+            return 0;
+        }
+        else 
+        {
+            return 1;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player")) 
+        if (collision.gameObject.CompareTag("Player") && !move) 
         {
             //最初のムーブ プレイヤーの高さまでなど
             move = true;
             rb.AddForce(new Vector2(0, -3), ForceMode2D.Impulse);
-            Debug.Log("first"); 
         }
     }
 }
