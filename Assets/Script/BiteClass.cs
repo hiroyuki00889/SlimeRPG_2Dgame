901e9a760b9e6bde02f3c6208ac942b43738dce4
hiroyuki00889 <murasansei8_13@yahoo.co.jp>
2023-10-10 17:55:04 +0900

    SkillTable・Animation/タグ・BiteClass・Resources編集

--- Assets/Script/BiteClass.cs
+++ Assets/Script/BiteClass.cs
@@ -18,13 +18,16 @@ public class BiteClass : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         if (right)
         {
+            gameObject.transform.localScale = Vector3.one;
             rb.AddForce(new Vector3(10, 0, 0), ForceMode2D.Impulse);
         }
         else
         {
+            gameObject.transform.localScale = new Vector3(-1,1,1);
             rb.AddForce(new Vector3(-10, 0, 0), ForceMode2D.Impulse);
         }
-        Invoke(("Destroy1"), 2f);
+        Invoke(("Stop"),0.5f);
+        Invoke(("Destroy1"), 1f);
     }
 
     private void Destroy1()
@@ -33,5 +36,17 @@ public class BiteClass : MonoBehaviour
         Destroy(this.gameObject);
     }
 
-    
+    private void Stop()
+    {
+        rb.velocity = Vector2.zero;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Bunny")|| collision.gameObject.CompareTag("Dog")|| collision.gameObject.CompareTag("Bat")
+            || collision.gameObject.CompareTag("Opossum"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
 }
