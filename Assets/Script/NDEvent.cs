3c117b37ada3bb19974fe785817fdc7c3b9b1778
Valkyrja <valkyrja45queen@outlook.jp>
2023-11-25 15:10:48 +0900

    event

--- Assets/Script/NDEvent.cs
+++ Assets/Script/NDEvent.cs
@@ -10,12 +10,46 @@ public class NDEvent : MonoBehaviour
     [SerializeField] AnimateNDialog animateNDialog;
     bool isCallOnece = false;
     public Text ndEventText;
-    int i=0;
+    int i = 0;
     public bool isNDEvent = false;
     public bool FirstEvent;//チェックありなしで表示する文章を選択
 
     string[] words = {"魔物の縄張りに住むスライムは平和に暮らしていました",
         "しかし、あることで縄張りを治める四天王にムカついてしまいました","スライムは四天王を倒す旅に出ます"};
+
+    ////辞書定義
+    //private Dictionary<string, string> ndEvent = new Dictionary<string, string>();
+
+    //private void Start()
+    //{
+    //    ndEvent.Add("EventTagA", "イベントタグAの文章です");
+    //    ndEvent.Add("EventTagB", "イベントタグBの文章です");
+    //    ndEvent.Add("EventTagC", "イベントタグCの文章です");
+    //    ndEvent.Add("EventTagD", "イベントタグDの文章です");
+    //    // ここに会話文追加
+    //}
+
+    //public void StartNDEvent(string eventTag)
+    //{
+    //    // コライダーのタグで会話内容を取得
+    //    if(ndEvent.TryGetValue(eventTag, out string ndEventText))
+    //    {
+    //        // 会話内容を表示
+    //        DisplayNDEvent(ndEventText);
+    //    }
+    //    //else
+    //    //{
+    //    //    エラーハンドリングの内容をここに記述
+    //    //}
+    //}
+
+    //void DisplayNDEvent(string text)
+    //{
+    //    // 会話内容をUIに表示する処理
+    //    ndEventText.text = text;
+    //    // 文章を送って表示する処理を以下に記述
+    //    // 思いつきません
+    //}
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
---
+++ Assets/Script/NDEventTrigger.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//public class NDEventTrigger : MonoBehaviour
+//{
+//    public string eventTag;
+//    public NDEvent nDEvent;
+
+//    private void OnTriggerEnter2D(Collider2D collision)
+//    {
+//        if(collision.CompareTag("Player"))
+//        {
+//            nDEvent = collision.gameObject.GetComponent<NDEvent>();
+//            nDEvent.StartNDEvent(eventTag);
+//        }
+//    }
+//}
