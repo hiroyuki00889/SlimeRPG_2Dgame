db2845f78b74348a98312b91b09cce86a1230da9
斎藤 <2201100257mn@cyber-u.ac.jp>
2023-12-11 20:33:43 +0900

    加速処理お試し

--- Assets/Script/BearScrollEvent.cs
+++ Assets/Script/BearScrollEvent.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class BearScrollEvent : MonoBehaviour
 {
-    public GameObject background;
+    public GameObject bear;
+    public Vector3 scrollspeed= new Vector3(0.01f, 0, 0);
+    private float time = 0f;
     private void Update() 
     {
-        background.transform.position += new Vector3(0.01f,0,0);
+        time+=Time.deltaTime;
+        this.transform.position += scrollspeed;
+        if (time>3) 
+        {
+            bear.gameObject.transform.position +=scrollspeed;
+        }
     }
+
 }
